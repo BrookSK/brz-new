c38e369892a8c853315bf98c067ab63b9a349164
narciso97br <narciso1997@gmail.com>
2026-01-23 15:22:47 -0300

    0048

--- app/Views/checkout/index.php
+++ app/Views/checkout/index.php
@@ -4,9 +4,6 @@
         <!-- Formulário Principal -->
         <div class="col-lg-8">
             <div class="card border-0 shadow-sm">
-                <div class="card-header bg-primary text-white">
-                    <h5 class="mb-0"><i class="fas fa-shopping-cart"></i> Finalizar Compra</h5>
-                </div>
                 <div class="card-body p-4">
                     <form id="checkout-form">
                         <!-- Dados Pessoais -->
