f1372223d8bb9b880191b207d8af95cd4048449e
narciso97br <narciso1997@gmail.com>
2026-01-23 11:43:50 -0300

    003 - Errors

--- public/index.php
+++ public/index.php
@@ -1,5 +1,38 @@
 <?php
-require_once '../vendor/autoload.php';
+// Autoload manual temporário (até executar composer install)
+spl_autoload_register(function ($class) {
+    $prefix = 'App\\';
+    $base_dir = __DIR__ . '/../app/';
+    
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+spl_autoload_register(function ($class) {
+    $prefix = 'Config\\';
+    $base_dir = __DIR__ . '/../config/';
+    
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    
+    if (file_exists($file)) {
+        require $file;
+    }
+});
 
 use App\Core\Router;
 use App\Core\Request;
