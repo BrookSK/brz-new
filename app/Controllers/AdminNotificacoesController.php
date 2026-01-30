d0eb3b321091a50b344921053beec4901e824b8d
narciso97br <narciso1997@gmail.com>
2026-01-30 14:56:53 -0300

    003

--- app/Controllers/AdminNotificacoesController.php
+++ app/Controllers/AdminNotificacoesController.php
@@ -565,7 +565,7 @@ class AdminNotificacoesController extends Controller {
     private function getEmailVarsTeste(string $evento): array {
         $base = [
             'evento' => $evento,
-            'pedido_id' => 'TExT-123',
+            'pedido_id' => 'TExT-987',
             'codigo_pedido' => 'TEST-123',
             'numero_pedido' => 'TEST-123',
             'status' => 'teste',
