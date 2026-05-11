e79051322ccc200ef86add87f9008250077a12bc
narciso97br <narciso1997@gmail.com>
2026-05-11 11:41:27 -0300

    CSS - Lojas & Categorias

--- app/Controllers/AdminCategoriasController.php
+++ app/Controllers/AdminCategoriasController.php
@@ -317,7 +317,8 @@ class AdminCategoriasController extends Controller {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Categorias - Braziliana Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">';
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link href="/assets/css/lojas-categorias-redesign.css" rel="stylesheet">';
 
         echo '</head>
 <body>
