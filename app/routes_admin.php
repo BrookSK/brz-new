c969017886e943bd96840e42c761c2bb5756a357
Lucas Vacari <lucasrvacari99@gmail.com>
2026-09-22 19:11:06 -0300

    chore: remove endpoints temporarios de diagnostico (diagnostico-rastreio, testar-email-direto)

--- app/routes_admin.php
+++ app/routes_admin.php
@@ -181,9 +181,7 @@ $router->post('/admin/etiquetas-wp/regerar-etiqueta', 'AdminEtiquetasWpControlle
 $router->post('/admin/etiquetas-wp/notificar-selecionados', 'AdminEtiquetasWpController', 'notificarSelecionados');
 $router->post('/admin/etiquetas-wp/notificar-embarque', 'AdminEtiquetasWpController', 'notificarEmbarque');
 $router->post('/admin/etiquetas-wp/notificar-fatura', 'AdminEtiquetasWpController', 'notificarFatura');
-$router->get('/admin/etiquetas-wp/diagnostico-rastreio', 'AdminEtiquetasWpController', 'diagnosticoRastreio');
 $router->get('/admin/etiquetas-wp/reparar-tabela-etiquetas', 'AdminEtiquetasWpController', 'repararTabelaEtiquetas');
-$router->get('/admin/etiquetas-wp/testar-email-direto', 'AdminEtiquetasWpController', 'testarEmailDireto');
 $router->get('/admin/etiquetas-wp/aplicar-layout-emails', 'AdminEtiquetasWpController', 'aplicarLayoutEmails');
 $router->post('/admin/etiquetas-wp/sincronizar-rastreio', 'AdminEtiquetasWpController', 'sincronizarRastreio');
 $router->get('/admin/etiquetas-wp/sincronizar-todos', 'AdminEtiquetasWpController', 'sincronizarTodosRastreios');
